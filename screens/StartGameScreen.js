a06f7003ab858a828a5adeec40f5b3f4271f9326
y-noda <y-noda@entaworks.co.jp>
2020-12-03 09:59:27 +0900

    90終了

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -109,8 +109,10 @@ const styles = StyleSheet.create({
     fontFamily: 'open-sans-bold'
   },
   inputContainer: {
-    width: 300,
-    maxWidth: '80%',
+    width: '80%',
+    // maxWidth: '80%',
+    maxWidth: '95%',
+    minWidth: 300,
     alignItems: 'center',
   },
   buttonContainer: {
